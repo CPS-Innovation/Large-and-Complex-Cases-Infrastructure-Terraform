eb7a2dfc138ada42a12442666d318a8cac730289
Bun Camara <bun.camara@cps.gov.uk>
2025-04-14 16:19:31 +0100

    create app service plan

---
+++ environments/dev/applications/app-service-plan.tf
@@ -0,0 +1,8 @@
+module "ui-app-service-plan" {
+  source = "../../../modules/app-service-plan"
+
+  main_rg        = module.dev-rg.name
+  location       = var.location
+  environment    = var.environment
+  appsrvpln_name = "asp-hsk-ui-${var.environment}"
+}
