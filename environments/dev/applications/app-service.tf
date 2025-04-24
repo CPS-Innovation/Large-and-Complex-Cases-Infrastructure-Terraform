a9671e8c498b1489059248dc1623cd2e85682e03
Bun Camara <bun.camara@cps.gov.uk>
2025-04-24 09:43:10 +0100

    change subnet

--- environments/dev/applications/app-service.tf
+++ environments/dev/applications/app-service.tf
@@ -6,7 +6,7 @@ module "ui-spa" {
   name_of_fa       = "fa-lacc-${var.environment}"
   name_of_pep      = "fa-lacc-${var.environment}-pep"
   environment      = var.environment
-  pep_subnet_id    = data.azurerm_subnet.base["subnet-lacc-service-apps-staging"].id
+  pep_subnet_id    = data.azurerm_subnet.base["subnet-lacc-service-staging"].id
   subresource_name = ["sites"]
 
   ui_service_plan_id        = module.ui-app-service-plan.id
