d619e9a0ca9be2c11e74c875c1e494e9e305af74
Bun Camara <bun.camara@cps.gov.uk>
2025-05-02 15:24:25 +0100

    import ui

--- environments/dev/applications/ui-spa.tf
+++ environments/dev/applications/ui-spa.tf
@@ -79,3 +79,8 @@ resource "azurerm_private_endpoint" "pep_ui_web_app" {
     ignore_changes = all
   }
 }
+
+import {
+    id = "/subscriptions/7f67e716-03c5-4675-bad2-cc5e28652759/resourcegroups/rg-lacc-dev/providers/microsoft.network/privateendpoints/lacc-app-ui-spa-dev-pe"
+    to = azurerm_linux_web_app.ui_spa
+}
