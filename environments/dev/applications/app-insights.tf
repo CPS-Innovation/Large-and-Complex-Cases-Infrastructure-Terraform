8ea79fc7131ae71ca5c1a6394cca6a82f5fe4d57
Bun Camara <bun.camara@cps.gov.uk>
2025-06-16 12:58:17 +0100

    private end point for ampls

--- environments/dev/applications/app-insights.tf
+++ environments/dev/applications/app-insights.tf
@@ -17,30 +17,28 @@ resource "azurerm_monitor_private_link_scope" "preprod" {
   query_access_mode     = "Open" #"PrivateOnly"
 }
 
-#  resource "azurerm_private_endpoint" "pep_ampls" {
-#   name                = "mpls-pe-lacc-${var.environment}"
-#   location            = module.dev-rg.location
-#   resource_group_name = module.dev-rg.name
-#   subnet_id           = data.azurerm_subnet.base["subnet-hsk-service-dev"].id
+resource "azurerm_private_endpoint" "pep_ampls" {
+  name                = "mpls-pe-lacc-${var.environment}"
+  location            = module.dev-rg.location
+  resource_group_name = module.dev-rg.name
+  subnet_id           = data.azurerm_subnet.base["subnet-hsk-service-dev"].id
 
-#   private_service_connection {
-#     name                           = "mpls-pe-lacc-preprod-${var.environment }"
-#     private_connection_resource_id = azurerm_monitor_private_link_scope.preprod.id
-#     subresource_names              = ["azuremonitor"]
-#     is_manual_connection           = false
-#   }
+  private_service_connection {
+    name                           = "mpls-pe-lacc-preprod-${var.environment}"
+    private_connection_resource_id = azurerm_monitor_private_link_scope.preprod.id
+    subresource_names              = ["azuremonitor"]
+    is_manual_connection           = false
+  }
 
-#   private_dns_zone_group {
-#     name                 = "default"
-#     private_dns_zone_ids = [azurerm_private_dns_zone.rg_dev_ampls.id]
-#   }
+  private_dns_zone_group {
+    name                 = "default"
+    private_dns_zone_ids = [data.azurerm_private_dns_zone.monitoring_lacc_connectivity.id]
+  }
 
-#   tags = {
-#     environment : var.environment
-#   }
-
-#   depends_on = [ azurerm_private_dns_zone.rg_dev_ampls ]
-# }
+  tags = {
+    environment : var.environment
+  }
+}
 
 resource "azurerm_log_analytics_workspace" "dev" {
   name                = "log-analytics-lacca-${var.environment}"
