ec08a50cd9eb059a6435cef6cf8e35b58e8ee4b4
Lilach.Davis <Lilach.Davis@cps.gov.uk>
2025-07-02 17:46:45 +0100

    use loop for role assignments

--- environments/dev/applications/iam.tf
+++ environments/dev/applications/iam.tf
@@ -1,23 +1,15 @@
-resource "azurerm_role_assignment" "filetransfer_st_blob_data_owner" {
+resource "azurerm_role_assignment" "filetransfer_sa" {
+  for_each = var.fa_sa_roles
+
   scope                = module.sa_dev.id
-  role_definition_name = "Storage Blob Data Owner"
+  role_definition_name = each.value
   principal_id         = azurerm_windows_function_app.filetransfer.identity[0].principal_id
 }
 
-resource "azurerm_role_assignment" "filetransfer_st_table_data_cont" {
-  scope                = module.sa_dev.id
-  role_definition_name = "Storage Table Data Contributor"
-  principal_id         = azurerm_windows_function_app.filetransfer.identity[0].principal_id
-}
+resource "azurerm_role_assignment" "fa_main_sa" {
+  for_each = var.fa_sa_roles
 
-resource "azurerm_role_assignment" "fa_main_st_blob_data_owner" {
   scope                = module.sa_dev.id
-  role_definition_name = "Storage Blob Data Owner"
-  principal_id         = azurerm_windows_function_app.fa_main.identity[0].principal_id
-}
-
-resource "azurerm_role_assignment" "fa_main_st_table_data_cont" {
-  scope                = module.sa_dev.id
-  role_definition_name = "Storage Table Data Contributor"
+  role_definition_name = each.value
   principal_id         = azurerm_windows_function_app.fa_main.identity[0].principal_id
 }
--- environments/dev/applications/terraform.tfvars
+++ environments/dev/applications/terraform.tfvars
@@ -2,3 +2,4 @@ location    = "UK South"
 environment = "dev"
 vnet_rg     = "RG-LaCC-connectivity"
 vnet_name   = "VNET-LaCC-WANNET"
+fa_sa_roles = ["Storage Blob Data Owner", "Storage Table Data Contributor"]
