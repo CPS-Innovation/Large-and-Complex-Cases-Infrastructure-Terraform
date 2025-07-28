f227b6dc616f7d60bb90cb3407b3b70b4ad7dcfb
Lilach <Lilach.Davis@cps.gov.uk>
2025-07-28 15:34:08 +0000

    remove modules directory as no longer in use (#51)

--- environments/dev/applications/app-service-plan.tf
+++ environments/dev/applications/app-service-plan.tf
@@ -5,7 +5,7 @@ resource "azurerm_service_plan" "linux" {
   os_type                      = "Linux"
   sku_name                     = var.app_asp_sku
   maximum_elastic_worker_count = var.app_asp_max_elastic_worker_count
-  zone_balancing_enabled       = true
+  zone_balancing_enabled       = false
   worker_count                 = var.app_asp_worker_count
 
   tags = local.tags
