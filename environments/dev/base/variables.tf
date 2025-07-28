42c401314ec371e0a06338d0d0850d9bfb7fd266
Lilach.Davis <Lilach.Davis@cps.gov.uk>
2025-07-28 12:22:01 +0100

    add common subnet and preprod nsg

--- environments/dev/base/variables.tf
+++ environments/dev/base/variables.tf
@@ -1,3 +1,12 @@
+variable "subscription_env" {
+  type        = string
+  description = "The subscription's environment. Possible values are preprod and prod."
+  validation {
+    condition     = contains(["preprod", "prod"], var.subscription_env)
+    error_message = "Invalid input for subscription_name. Possible values are preprod and prod."
+  }
+}
+
 variable "vnet_name" {
   type        = string
   description = "The name of the virtual network in which to create the subnet"
@@ -6,11 +15,6 @@ variable "vnet_name" {
 variable "vnet_rg" {
   type        = string
   description = "The name of the virtual network in which to create the subnet"
-}
-
-variable "nsg_lacc_name" {
-  type        = string
-  description = "Name of the Network Security Group"
 }
 
 variable "rt_lacc_name" {
@@ -25,4 +29,27 @@ variable "subnets" {
     service_delegation = bool
   }))
   description = "A map of subnet names to their properties."
+}
+
+variable "create_nsg" {
+  type    = bool
+  default = true
+}
+
+variable "nsg_rules" {
+  type = map(object({
+    priority                     = number
+    direction                    = string
+    access                       = string
+    protocol                     = string
+    source_port_range            = optional(string)
+    source_port_ranges           = optional(list(string))
+    destination_port_range       = optional(string)
+    destination_port_ranges      = optional(list(string))
+    source_address_prefix        = optional(string)
+    source_address_prefixes      = optional(list(string))
+    destination_address_prefix   = optional(string)
+    destination_address_prefixes = optional(list(string))
+  }))
+  description = "A map of NSG rules to their properties. For destination and source addresses, either 'prefix' or 'prefixes' must be present."
 }
