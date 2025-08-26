27a19b19f2d13143172dc587c2e0c027994b071a
Lilach <Lilach.Davis@cps.gov.uk>
2025-08-26 13:42:45 +0100

    disable public network access to monitoring resources (#61)

--- environments/dev/applications/app-insights.tf
+++ environments/dev/applications/app-insights.tf
@@ -1,17 +1,21 @@
 resource "azurerm_application_insights" "app_insights" {
-  name                = "ai-lacc-${var.environment}"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  application_type    = "web"
-  workspace_id        = azurerm_log_analytics_workspace.law.id
+  name                       = "ai-lacc-${var.environment}"
+  location                   = azurerm_resource_group.rg.location
+  resource_group_name        = azurerm_resource_group.rg.name
+  application_type           = "web"
+  workspace_id               = azurerm_log_analytics_workspace.law.id
+  internet_ingestion_enabled = false
+  internet_query_enabled     = false
 }
 
 resource "azurerm_log_analytics_workspace" "law" {
-  name                = "log-analytics-lacc-${var.environment}"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  sku                 = "PerGB2018"
-  retention_in_days   = 60
+  name                       = "log-analytics-lacc-${var.environment}"
+  location                   = azurerm_resource_group.rg.location
+  resource_group_name        = azurerm_resource_group.rg.name
+  sku                        = "PerGB2018"
+  retention_in_days          = 60
+  internet_ingestion_enabled = false
+  internet_query_enabled     = false
 }
 
 resource "azurerm_monitor_private_link_scoped_service" "law" {
