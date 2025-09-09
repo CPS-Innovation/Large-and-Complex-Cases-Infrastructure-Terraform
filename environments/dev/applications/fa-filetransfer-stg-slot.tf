275b210674a32099c8eb9ca44a2b1bbc639f47e7
Lilach <Lilach.Davis@cps.gov.uk>
2025-09-09 16:11:38 +0100

    Add staging slots for all apps in Dev (#65)

---
+++ environments/dev/applications/fa-filetransfer-stg-slot.tf
@@ -0,0 +1,48 @@
+resource "azurerm_windows_function_app_slot" "filetransfer_stg" {
+  name                 = "stg"
+  function_app_id      = azurerm_windows_function_app.filetransfer.id
+  storage_account_name = azurerm_storage_account.sa.name
+
+  storage_uses_managed_identity = true
+  public_network_access_enabled = false
+  virtual_network_subnet_id     = data.azurerm_subnet.base["subnet-lacc-windows-apps-${var.environment}"].id
+  builtin_logging_enabled       = false
+  https_only                    = true
+
+  site_config {}
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  tags = local.tags
+
+  lifecycle {
+    ignore_changes = [
+      app_settings,
+      tags,
+      site_config,
+    ]
+  }
+}
+
+resource "azurerm_private_endpoint" "filetransfer_stg" {
+  name                = "pe-fa-lacc-filetransfer-${var.environment}-stg"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  subnet_id           = data.azurerm_subnet.base["subnet-lacc-service-${var.environment}"].id
+
+  private_service_connection {
+    name                           = "fa-lacc-filetransfer-${var.environment}-stg"
+    private_connection_resource_id = azurerm_windows_function_app_slot.filetransfer_stg.id
+    subresource_names              = ["sites-stg"]
+    is_manual_connection           = false
+  }
+
+  private_dns_zone_group {
+    name                 = "fa-filetransfer-dns-zone-group"
+    private_dns_zone_ids = [data.azurerm_private_dns_zone.lacc_connectivity["sites"].id]
+  }
+
+  tags = local.tags
+}
