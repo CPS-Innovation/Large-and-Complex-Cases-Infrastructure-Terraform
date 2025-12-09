7def7e706f68bec70ba136b5e269242f67b305d5
Lilach.Davis <Lilach.Davis@cps.gov.uk>
2025-12-09 11:52:20 +0000

    add diagnostic setting

--- environments/staging/applications/diagnostics.tf
+++ environments/staging/applications/diagnostics.tf
@@ -2,7 +2,7 @@ resource "azurerm_monitor_diagnostic_setting" "ai_to_evh_siem" {
   name                           = "ai-to-siem"
   target_resource_id             = azurerm_application_insights.app_insights.id
   eventhub_name                  = "eh-siem-01"
-  eventhub_authorization_rule_id = data.azurerm_eventhub_namespace_authorization_rule.evhns_siem.id
+  eventhub_authorization_rule_id = "/subscriptions/9d2e7ffe-ad72-4bfe-ad8f-4932730c0f39/resourceGroups/rg-siem-eventhub/providers/Microsoft.EventHub/namespaces/ns-siem-eventhub/authorizationRules/eh-siem-sap-01"
 
   dynamic "enabled_log" {
     for_each = ["AppEvents", "AppExceptions", "AppPageViews", "AppRequests", "AppSystemEvents", "AppTraces"]
