d8c1498bf564ba9321dfbebf1db3a3642f48764b
Lilach <Lilach.Davis@cps.gov.uk>
2026-01-12 16:12:02 +0000

    task/FCT2-9430 Custom domains for Prod (#100)

---
+++ environments/prod/applications/custom-domain.tf
@@ -0,0 +1,39 @@
+locals {
+  custom_hostnames = {
+    lcc = {
+      app_service     = azurerm_linux_web_app.ui_spa
+      functional_area = "ui"
+    }
+    lcc-api = {
+      app_service     = azurerm_windows_function_app.fa_main
+      functional_area = "api"
+    }
+  }
+}
+
+data "azurerm_app_service_certificate" "cert" {
+  # purchased and added manually to the app service instances
+  for_each = local.custom_hostnames
+
+  name                = "cert-lacc-${each.value.functional_area}-${var.environment}"
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_app_service_custom_hostname_binding" "hostname" {
+  # TXT records added manually to cps.gov.uk DNS zone to enable domain verification
+  for_each            = local.custom_hostnames
+  hostname            = "${each.key}.cps.gov.uk"
+  app_service_name    = each.value.app_service.name
+  resource_group_name = azurerm_resource_group.rg.name
+
+  lifecycle {
+    ignore_changes = [ssl_state, thumbprint]
+  }
+}
+
+resource "azurerm_app_service_certificate_binding" "hostname" {
+  for_each            = local.custom_hostnames
+  hostname_binding_id = azurerm_app_service_custom_hostname_binding.hostname[each.key].id
+  certificate_id      = data.azurerm_app_service_certificate.cert[each.key].id
+  ssl_state           = "SniEnabled"
+}
