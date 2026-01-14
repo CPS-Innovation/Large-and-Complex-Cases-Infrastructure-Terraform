c79702a788f30db259929d8ea68a735daacbd496
Lilach.Davis <Lilach.Davis@cps.gov.uk>
2026-01-14 14:49:13 +0000

    remove cors from ignore_changes

--- environments/prod/applications/fa-main.tf
+++ environments/prod/applications/fa-main.tf
@@ -55,7 +55,6 @@ resource "azurerm_windows_function_app" "fa_main" {
     ignore_changes = [
       app_settings,
       tags,
-      site_config[0].cors[0].allowed_origins
     ]
     # this needs to be in place to stop the app_setting been replaced as it is set in the pipeline and also to make the application stable. If any
     # changes needs to be made to the application via terraform, change the lifecycle value to [ app_settings ]
