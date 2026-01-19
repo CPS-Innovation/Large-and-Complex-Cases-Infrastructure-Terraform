7022c097c5d26eada27df6206276443c1f65b4a8
Lilach <Lilach.Davis@cps.gov.uk>
2026-01-19 11:33:16 +0000

    Task/fct2-13819 upgrade azurerm provider version (#107)

--- environments/prod/applications/versions.tf
+++ environments/prod/applications/versions.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "4.23"
+      version = "4.57"
     }
     azuread = {
       source  = "hashicorp/azuread"
