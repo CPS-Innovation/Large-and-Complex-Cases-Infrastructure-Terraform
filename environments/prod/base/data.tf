0a858caad2a13f7f50f71452a8e31a1c6f381b21
Lilach <Lilach.Davis@cps.gov.uk>
2026-06-05 17:05:04 +0100

    consolidate vnet rg references

--- environments/prod/base/data.tf
+++ environments/prod/base/data.tf
@@ -5,11 +5,11 @@ data "azurerm_virtual_network" "vnet-lacc" {
 
 data "azurerm_route_table" "lacc-rt" {
   name                = var.rt_lacc_name
-  resource_group_name = var.vnet_rg
+  resource_group_name = data.azurerm_virtual_network.vnet-lacc.resource_group_name
 }
 
 data "azurerm_network_security_group" "nsg" {
   count               = var.create_nsg ? 0 : 1
   name                = "nsg-lacc-${var.subscription_env}"
-  resource_group_name = var.vnet_rg
+  resource_group_name = data.azurerm_virtual_network.vnet-lacc.resource_group_name
 }
