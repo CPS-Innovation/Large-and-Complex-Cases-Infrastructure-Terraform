894e18f982f7a82bf9142b97f362ac1a7a146404
Lilach <Lilach.Davis@cps.gov.uk>
2026-06-17 16:55:40 +0100

    create new KVs

--- environments/staging/applications/kv-fa.tf
+++ environments/staging/applications/kv-fa.tf
@@ -38,3 +38,46 @@ resource "azurerm_private_endpoint" "kv" {
 
   tags = local.tags
 }
+
+resource "azurerm_key_vault" "kv-api" {
+  #checkov:skip=CKV_AZURE_42:Ensure the key vault is recoverable
+  #checkov:skip=CKV_AZURE_110:Ensure that key vault enables purge protection
+  name                = "kv-lacc-api-${var.environment}"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = var.location
+  tenant_id           = data.azurerm_client_config.current.tenant_id
+  sku_name            = lower(var.kv_sku)
+
+  rbac_authorization_enabled    = true
+  public_network_access_enabled = false
+  purge_protection_enabled      = var.kv_purge_protection_enabled
+  soft_delete_retention_days    = var.kv_soft_delete_retention_days
+
+  network_acls {
+    bypass         = "None"
+    default_action = "Deny"
+  }
+
+  tags = local.tags
+}
+
+resource "azurerm_private_endpoint" "kv-api" {
+  name                = "pe-${azurerm_key_vault.kv-api.name}"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  subnet_id           = data.azurerm_subnet.base["subnet-lacc-service-${var.environment}"].id
+
+  private_service_connection {
+    name                           = azurerm_key_vault.kv-api.name
+    private_connection_resource_id = azurerm_key_vault.kv-api.id
+    subresource_names              = ["vault"]
+    is_manual_connection           = false
+  }
+
+  private_dns_zone_group {
+    name                 = "kv-dns-zone-group"
+    private_dns_zone_ids = [data.azurerm_private_dns_zone.lacc_connectivity["vault"].id]
+  }
+
+  tags = local.tags
+}
