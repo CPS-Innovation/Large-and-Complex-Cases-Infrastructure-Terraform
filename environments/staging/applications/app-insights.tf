e0dc97ffebd537313ccaac19af5fa198b10195ab
Lilach <Lilach.Davis@cps.gov.uk>
2026-08-10 15:29:01 +0100

    Task/FCT2-21220 exclude status endpoint from app insights telemetry (#194)
    add workspace transformation DCR to filter out health check logs

--- environments/staging/applications/app-insights.tf
+++ environments/staging/applications/app-insights.tf
@@ -16,4 +16,7 @@ resource "azurerm_log_analytics_workspace" "law" {
   retention_in_days          = var.log_retention_days
   internet_ingestion_enabled = false
   internet_query_enabled     = false
+
+  # Not using resource reference to the DCR as this creates a dependency cycle
+  data_collection_rule_id = "${azurerm_resource_group.rg.id}/providers/Microsoft.Insights/dataCollectionRules/dcr-xform-lacc-${var.environment}"
 }
