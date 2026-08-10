02c40145f90fe1db57d01a0a32cd5d0d53869d3d
Lilach <Lilach.Davis@cps.gov.uk>
2026-08-10 12:45:27 +0000

    associate dcr with LAW

--- environments/prod/applications/dcr-log-transform.tf
+++ environments/prod/applications/dcr-log-transform.tf
@@ -1,13 +1,19 @@
+locals {
+  law_name = "log-analytics-lacc-${var.environment}"
+}
+
 resource "azurerm_monitor_data_collection_rule" "law_transform" {
   name                = "dcr-xform-lacc-${var.environment}"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   kind                = "WorkspaceTransforms"
 
+  data_sources {}
+
   destinations {
     log_analytics {
-      workspace_resource_id = azurerm_log_analytics_workspace.law.id
-      name                  = azurerm_log_analytics_workspace.law.name
+      workspace_resource_id = "${azurerm_resource_group.rg.id}/providers/Microsoft.OperationalInsights/workspaces/${local.law_name}"
+      name                  = local.law_name
     }
   }
 
@@ -31,7 +37,7 @@ resource "azurerm_monitor_data_collection_rule" "law_transform" {
     }
     content {
       streams       = data_flow.value.streams
-      destinations  = [azurerm_log_analytics_workspace.law.name]
+      destinations  = [local.law_name]
       transform_kql = data_flow.value.transform_kql
     }
   }
