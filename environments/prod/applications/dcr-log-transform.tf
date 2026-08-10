d3283ebc4f9a58d1a1d5abafe13c42bed97e3ab8
Lilach <Lilach.Davis@cps.gov.uk>
2026-08-10 16:49:31 +0100

    fix/FCT2-21220 exclude status endpoint from app insights telemetry (#195)

--- environments/prod/applications/dcr-log-transform.tf
+++ environments/prod/applications/dcr-log-transform.tf
@@ -1,3 +1,12 @@
+# Not using resource reference to the Log Analytics Workspace
+# as it creates a dependency cycle when the DCR is referenced
+# by the workspace resource attribute data_collection_rule_id
+
+locals {
+  law_name        = "log-analytics-lacc-${var.environment}"
+  law_resource_id = "${azurerm_resource_group.rg.id}/providers/Microsoft.OperationalInsights/workspaces/${local.law_name}"
+}
+
 resource "azurerm_monitor_data_collection_rule" "law_transform" {
   name                = "dcr-xform-lacc-${var.environment}"
   resource_group_name = azurerm_resource_group.rg.name
@@ -8,8 +17,8 @@ resource "azurerm_monitor_data_collection_rule" "law_transform" {
 
   destinations {
     log_analytics {
-      workspace_resource_id = azurerm_log_analytics_workspace.law.id
-      name                  = azurerm_log_analytics_workspace.law.name
+      workspace_resource_id = local.law_resource_id
+      name                  = local.law_name
     }
   }
 
@@ -33,7 +42,7 @@ resource "azurerm_monitor_data_collection_rule" "law_transform" {
     }
     content {
       streams       = data_flow.value.streams
-      destinations  = [azurerm_log_analytics_workspace.law.name]
+      destinations  = [local.law_name]
       transform_kql = data_flow.value.transform_kql
     }
   }
