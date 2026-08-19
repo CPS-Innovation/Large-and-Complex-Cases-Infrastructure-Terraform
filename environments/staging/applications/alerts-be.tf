e9dca8346001fcd619f46004252e5c40dcb3aca2
Lilach <Lilach.Davis@cps.gov.uk>
2026-08-19 17:46:18 +0100

    Task/FCT2-21531 fix alert for storage blob service deletion events (#202)

--- environments/staging/applications/alerts-be.tf
+++ environments/staging/applications/alerts-be.tf
@@ -33,13 +33,13 @@ resource "azurerm_monitor_metric_alert" "blob_service_delete_ops" {
   name                = "alert-lacc-blob-delete-ops-${var.environment}"
   resource_group_name = azurerm_resource_group.rg.name
   description         = "A blob or container was deleted from storage account salacc${var.environment}."
-  scopes              = [azurerm_storage_account.sa.id]
+  scopes              = ["${azurerm_storage_account.sa.id}/blobServices/default"]
   severity            = 2
 
   criteria {
-    metric_namespace = "Microsoft.Storage/storageAccounts/blobServices"
+    metric_namespace = "Microsoft.Storage/storageAccounts/blobservices"
     metric_name      = "Transactions"
-    aggregation      = "Count"
+    aggregation      = "Total"
     operator         = "GreaterThan"
     threshold        = 0
 
