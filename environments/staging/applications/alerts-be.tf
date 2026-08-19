e5d97dbbe922312969784aab79e7844b9bb7b993
Lilach <Lilach.Davis@cps.gov.uk>
2026-08-19 14:25:27 +0000

    add metric alert to fire on blob service delete transactions

--- environments/staging/applications/alerts-be.tf
+++ environments/staging/applications/alerts-be.tf
@@ -28,3 +28,37 @@ resource "azurerm_monitor_metric_alert" "kv_throttling" {
 
   tags = local.tags
 }
+
+resource "azurerm_monitor_metric_alert" "blob_service_delete_ops" {
+  name                = "alert-lacc-blob-delete-ops-${var.environment}"
+  resource_group_name = azurerm_resource_group.rg.name
+  description         = "A blob or container was deleted from storage account salacc${var.environment}."
+  scopes              = [azurerm_storage_account.sa.id]
+  severity            = 2
+
+  criteria {
+    metric_namespace = "Microsoft.Storage/storageAccounts/blobServices"
+    metric_name      = "Transactions"
+    aggregation      = "Count"
+    operator         = "GreaterThan"
+    threshold        = 0
+
+    dynamic "dimension" {
+      for_each = toset(["DeleteBlob", "DeleteContainer", "DeleteFile"])
+      content {
+        name     = "ApiName"
+        operator = "Include"
+        values   = [dimension.value]
+      }
+    }
+  }
+
+  frequency   = "PT1M"
+  window_size = "PT5M"
+
+  action {
+    action_group_id = azurerm_monitor_action_group.api_alerts.id
+  }
+
+  tags = local.tags
+}
