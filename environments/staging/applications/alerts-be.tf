1360e65bacc02a91d400730ed10dbe54e3a3e6e2
Lilach <Lilach.Davis@cps.gov.uk>
2026-08-21 14:11:48 +0100

    Task/FCT2-21531 configure log query alert for storage blob service deletion events (#204)

--- environments/staging/applications/alerts-be.tf
+++ environments/staging/applications/alerts-be.tf
@@ -30,33 +30,84 @@ resource "azurerm_monitor_metric_alert" "kv_throttling" {
   tags = local.tags
 }
 
-resource "azurerm_monitor_metric_alert" "blob_service_delete_ops" {
+resource "azurerm_monitor_scheduled_query_rules_alert_v2" "blob_service_delete_ops" {
   name                = "alert-lacc-blob-delete-ops-${var.environment}"
   resource_group_name = azurerm_resource_group.rg.name
-  description         = "A blob or container was deleted from storage account salacc${var.environment}."
-  scopes              = ["${azurerm_storage_account.sa.id}/blobServices/default"]
-  severity            = 2
-  auto_mitigate       = true
+  location            = azurerm_resource_group.rg.location
+
+  display_name         = "Blob Service Delete Operation in ${var.environment}"
+  description          = "A blob or container was deleted from storage account salacc${var.environment}."
+  evaluation_frequency = "PT1M"
+  window_duration      = "PT5M"
+  scopes               = [azurerm_log_analytics_workspace.law.id]
+  severity             = 2
 
   criteria {
-    metric_namespace = "Microsoft.Storage/storageAccounts/blobservices"
-    metric_name      = "Transactions"
-    aggregation      = "Total"
-    operator         = "GreaterThan"
-    threshold        = 0
+    /*
+    For Storage Resource Provider (SRP) mediated operations,
+    StorageBlobLogs may not contain the originating requester's object ID.
+    The Azure Activity Log can be used to determine the initiating identity.
+    */
+    query = <<-KQL
+      StorageBlobLogs
+      | where Category has "StorageDelete"
+      | extend IsTrustedAccessOperation =
+          AuthenticationType == "TrustedAccess"
+          and
+          tostring(UserAgentHeader) startswith "SRP"
+      | extend
+          Caller_Identity = case(
+            isnotempty(RequesterObjectId), RequesterObjectId,
+            IsTrustedAccessOperation, "Check Activity Log (TrustedAccess/SRP)",
+            "Unknown"
+          )
+      | project
+          Time_Generated = TimeGenerated,
+          Operation_Name = OperationName,
+          Object_Key = ObjectKey,
+          Response_Status = StatusText,
+          Caller_Identity,
+          Caller_IP = CallerIpAddress,
+          Auth_Type = AuthenticationType
+    KQL
 
-    dimension {
-      name     = "ApiName"
-      operator = "StartsWith"
-      values   = ["Delete"]
+    dynamic "dimension" {
+      for_each = [
+        "Time_Generated",
+        "Operation_Name",
+        "Object_Key",
+        "Response_Status",
+        "Caller_Identity",
+        "Caller_IP",
+        "Auth_Type"
+      ]
+      content {
+        name     = dimension.value
+        operator = "Include"
+        values   = ["*"]
+      }
+    }
+
+    time_aggregation_method = "Count"
+    operator                = "GreaterThan"
+    threshold               = 0
+
+    failing_periods {
+      minimum_failing_periods_to_trigger_alert = 1
+      number_of_evaluation_periods             = 1
     }
   }
 
-  frequency   = "PT1M"
-  window_size = "PT5M"
+  auto_mitigation_enabled          = true
+  workspace_alerts_storage_enabled = false
+  enabled                          = true
 
   action {
-    action_group_id = azurerm_monitor_action_group.api_alerts.id
+    action_groups = [azurerm_monitor_action_group.api_alerts.id]
+  }
+
+  identity {
+    type = "SystemAssigned"
   }
 
   tags = local.tags
